c973c1ca5f142ef8548158f8c4b90cea0fbf1f85
anderoidy <anderoidy@gmail.com>
2021-11-21 05:03:03 -0300

    created component create product

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
@@ -8,10 +9,17 @@ const routes: Routes = [
   { 
   path: "",
   component: HomeComponent
-  },{
+  },
+  {
     path: "products",
     component: ProductCrudComponent
+  },
+  { 
+    path: "products/create",
+    component: ProductCreateComponent
+    
   }
+
 ];
 
 @NgModule({
