05a81e57817c770bd33b0042b0e216de2ec68143
anderoidy <anderoidy@gmail.com>
2021-11-20 23:53:49 -0300

    created compontent pruduct-crud

---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appFor]'
+})
+export class ForDirective  implements OnInit{
+
+  @Input('appForEm') numbers!: number[]
+
+  constructor(
+    private container: ViewContainerRef, 
+    private template: TemplateRef<any>) {     
+  }
+
+  ngOnInit(): void {
+      
+    console.log(this.numbers)
+    for(let number of this.numbers){
+      this.container.createEmbeddedView(
+        this.template, { $implicit: number})
+
+    }
+  }
+}
